61e093a8e05d121dd012c8aa44613c15200963d2
Fazel Gheibi <fazelgheibi2001@gmail.com>
2023-07-24 18:29:31 +0330

    init project

--- src/main/java/com/airbyte/daneshmal/security/UserLoader.java
+++ src/main/java/com/airbyte/daneshmal/security/UserLoader.java
@@ -1,8 +1,10 @@
 package com.airbyte.daneshmal.security;
 
 import com.airbyte.daneshmal.security.permission.Role;
+import com.airbyte.daneshmal.security.user.UserInformationRepository;
 import com.airbyte.daneshmal.security.user.UserInformationService;
 import com.airbyte.daneshmal.security.user.model.UserDTO;
+import com.airbyte.daneshmal.security.user.model.UserInformation;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -11,21 +13,23 @@ import org.springframework.stereotype.Component;
 public class UserLoader implements ApplicationRunner {
 
     private final UserInformationService userInformationService;
+    private final UserInformationRepository userInformationRepository;
 
-    public UserLoader(UserInformationService userInformationService) {
+    public UserLoader(UserInformationService userInformationService, UserInformationRepository userInformationRepository) {
         this.userInformationService = userInformationService;
+        this.userInformationRepository = userInformationRepository;
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         try {
-            userInformationService.getByUsername("09924664362");
+            userInformationService.getByUsername("09383740404");
         } catch (IllegalArgumentException usernameNotFoundException) {
-            UserDTO userDTO = new UserDTO();
+            UserInformation userDTO = new UserInformation();
             userDTO.setUsername("09383740404");
             userDTO.setPassword("airByte");
-            userDTO.setRole(Role.MANAGER.name());
-            userInformationService.save(userDTO);
+            userDTO.setRole(Role.MANAGER);
+            userInformationRepository.save(userDTO);
         }
     }
 }
